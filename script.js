2f9563a241f48017b8b9678b9bc1fe1a65bfcbdb
ErLions94 <114865892+ErLions94@users.noreply.github.com>
2024-01-25 17:27:38 +0100

    script.js

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const testoContainer = document.getElementById('testoContainer');
+const responsiveDiv = document.getElementById('responsiveDiv');
 
-  testoContainer.addEventListener('click', function () {
-    testoContainer.classList.toggle('espanso');
-    adattaTesto();
-  });
-
-  window.addEventListener('resize', function () {
-    adattaTesto();
-  });
-
-  function adattaTesto() {
-    const testoRidotto = document.querySelector('.testo-ridotto');
-    const testoCompleto = document.querySelector('.testo-completo');
-
-    if (testoContainer.classList.contains('espanso')) {
-      testoRidotto.style.display = 'none';
-      testoCompleto.style.display = 'block';
-    } else {
-      testoRidotto.style.display = 'block';
-      testoCompleto.style.display = 'none';
-    }
-  }
+responsiveDiv.addEventListener('click', function() {
+  responsiveDiv.classList.toggle('full-text');
 });
